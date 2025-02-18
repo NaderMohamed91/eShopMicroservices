c3b20ff34fd3ca1541adbde3b3906dfe7feb80b9
nelhenidi <nelhenidi@nwc.com.sa>
2025-02-18 11:59:10 +0300

    Adding BuildingBlocks for managing 'CQRS - Mapster' && Add 'CreateProdudt' endpoint

---
+++ eshop-microservices/Services/Catalog/Catalog.Api/DI/CustomDependancyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Api.DI;
+
+public static class CustomDependancyInjection
+{
+    public static void AddDependancyInjection(this IServiceCollection services)
+    {
+        services.AddCarter();
+        services.AddMediatR(config => 
+        {
+            config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+        });
+    }
+}
--- eshop-microservices/Services/Catalog/Catalog.Api/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,12 @@
+using Catalog.Api.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDependancyInjection();
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.MapGet("/", () => "Api is up and running !!!");
 
