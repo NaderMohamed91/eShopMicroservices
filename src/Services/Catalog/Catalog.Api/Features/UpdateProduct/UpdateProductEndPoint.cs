8109327711da401c6096669ec0559c19473c7ea6
nelhenidi <nelhenidi@nwc.com.sa>
2025-03-03 12:27:32 +0300

    Adding (updateProduct) endpoint

---
+++ src/Services/Catalog/Catalog.Api/Features/UpdateProduct/UpdateProductEndPoint.cs
@@ -0,0 +1,30 @@
+﻿
+using Catalog.Api.Features.GetProductById;
+
+namespace Catalog.Api.Features.UpdateProduct;
+
+public record UpdateProductRequset(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/updateProduct", async (UpdateProductRequset product, ISender sender) => 
+        {
+            var productCommandToUpdate = product.Adapt<UpdateProductCommandRequset>();
+
+            var updatedProduct = await sender.Send(productCommandToUpdate);
+
+            var response = updatedProduct.Adapt<UpdateProductResult>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductResult>(StatusCodes.Status201Created)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Features/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.Api.Features.UpdateProduct;
+
+public record UpdateProductCommandRequset(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<UpdateProductCommandResult>;
+
+public record UpdateProductCommandResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler(ILogger<UpdateProductCommandHandler> logger, IDocumentSession session)
+    : ICommandHandler<UpdateProductCommandRequset, UpdateProductCommandResult>
+{
+    public async Task<UpdateProductCommandResult> Handle(UpdateProductCommandRequset command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommandHandler.Handle called with {@command}", command);
+
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product is null) throw new ProductNotFoundException();
+
+        product.Name = command.Name;
+        product.Category = command.Category;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+
+        session.Update(product);
+        await session.SaveChangesAsync();
+
+        return new UpdateProductCommandResult(true);
+    }
+}
