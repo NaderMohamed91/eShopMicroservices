69f1f196889220bb5edc9ca1b2a068b33ddb49c5
nelhenidi <nelhenidi@nwc.com.sa>
2025-03-03 12:47:24 +0300

    Adding (deleteProduct) endpoint

---
+++ src/Services/Catalog/Catalog.Api/Features/DeleteProduct/DeleteProductEndPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.Api.Features.DeleteProduct;
+
+public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/deleteProduct/{productId}", async (Guid productId, ISender sender) => 
+        {
+            var response = await sender.Send(new DeleteProductCommandRequest(productId));
+
+            var result = response.Adapt(response);
+
+            return Results.Ok(result);
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteProductResult>(StatusCodes.Status201Created)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Features/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.Api.Features.DeleteProduct;
+
+public record DeleteProductCommandRequest(Guid ProductId) : ICommand<DeleteProductCommandResult>;
+
+public record DeleteProductCommandResult(bool IsSuccess);
+
+internal class DeleteProductHandler(ILogger<DeleteProductHandler> logger, IDocumentSession session)
+    : ICommandHandler<DeleteProductCommandRequest, DeleteProductCommandResult>
+{
+    public async Task<DeleteProductCommandResult> Handle(DeleteProductCommandRequest command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductHandler.Handle called with {@command}", command);
+
+        var product = await session.LoadAsync<Product>(command.ProductId, cancellationToken);
+
+        if (product is null) throw new ProductNotFoundException();
+
+        session.Delete(product);
+        await session.SaveChangesAsync();
+
+        return new DeleteProductCommandResult(true);
+    }
+}
